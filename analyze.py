28ee62cd35050ee5bc0816be2771ef3793765be0
zq9120 <zq9120@yeah.net>
2016-12-04 22:23:09 +0800

    update

--- analyze.py
+++ analyze.py
@@ -9,7 +9,7 @@ server_time = []
 for i in range(0, len(client_pacp)):
     packet = client_pacp[i]
     if hasattr(packet, 'load') and str(packet.load).startswith("SDN_SPY_D"):
-        print packet.load
+        print packet.time
         client_time.append(packet.time)
 
 print "==================="
@@ -17,14 +17,14 @@ print "==================="
 for i in range(0, len(server_pacp)):
     packet = server_pacp[i]
     if hasattr(packet, 'load') and str(packet.load).startswith("SDN_SPY_D"):
-        print packet.load
+        print packet.time
         server_time.append(packet.time)
 
 print "NO.\tP1_SEND\tP1_RECV\tP2_SEND\tP2_RECV"
 if len(client_time) == len(server_time):
     for i in range(0, len(client_time), 4):
-        print "P%d\t%d\t%d\t%d\t%d" % (
-            (i / 4), int(server_time[i] - client_time[i]), int(server_time[i + 1] - client_time[i + 1]),
-            int(server_time[i + 2] - client_time[i + 2]), int(server_time[i + 3] - client_time[i + 3]))
+        print "D%f\t%f\t%f\t%f\t%f" % (
+            (i / 4 + 1), (server_time[i] - client_time[i]), (server_time[i + 1] - client_time[i + 1]),
+            (server_time[i + 2] - client_time[i + 2]), (server_time[i + 3] - client_time[i + 3]))
 else:
     print "data mismatch"
