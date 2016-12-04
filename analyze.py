d3d397994212aa7a7e193acdb8a9fa1ae4e56ebf
zq9120 <zq9120@yeah.net>
2016-12-04 22:37:14 +0800

    update

--- analyze.py
+++ analyze.py
@@ -5,26 +5,23 @@ server_pacp = rdpcap("server.pcap")
 
 client_time = []
 server_time = []
+title_dict = ["DST_PORT", "SRC_PORT", "TOS", "SRC_IP", "DST_IP", "SRC_MAC"]
 
 for i in range(0, len(client_pacp)):
     packet = client_pacp[i]
     if hasattr(packet, 'load') and str(packet.load).startswith("SDN_SPY_D"):
-        print packet.time
         client_time.append(packet.time)
-
-print "==================="
 
 for i in range(0, len(server_pacp)):
     packet = server_pacp[i]
     if hasattr(packet, 'load') and str(packet.load).startswith("SDN_SPY_D"):
-        print packet.time
         server_time.append(packet.time)
 
-print "%10s%12s%12s%12s%12s" % ("NO.", "P1_SEND", "P1_RECV", "P2_SEND", "P2_RECV")
+print "%10s%15s%15s%15s%15s" % ("NO.", "P1_SEND", "P1_RECV", "P2_SEND", "P2_RECV")
 if len(client_time) == len(server_time):
     for i in range(0, len(client_time), 4):
-        print "%10s%12.2f%12.2f%12.2f%12.2f" % (
-            ("D" + str(i / 4 + 1)), (server_time[i] - client_time[i]) * 100000000,
+        print "%10s%15.2f%15.2f%15.2f%15.2f" % (
+            (title_dict[i / 4]), (server_time[i] - client_time[i]) * 100000000,
             (client_time[i + 1] - server_time[i + 1]) * 100000000,
             (server_time[i + 2] - client_time[i + 2]) * 100000000,
             (client_time[i + 3] - server_time[i + 3]) * 100000000)
