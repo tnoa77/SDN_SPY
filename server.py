610308a95d37d3508121792783b328a9dbbb48b9
zq9120 <zq9120@yeah.net>
2016-12-04 18:32:44 +0800

    update

--- server.py
+++ server.py
@@ -2,6 +2,12 @@ from scapy.all import *
 
 mac_dst = "00:00:00:00:00:02"
 
+listenAddress = ('10.0.0.1', 9250)
+socketServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socketServer.bind(listenAddress)
+
 while True:
-    receive = sniff(filter="udp")
-    receive[0]
+    data, clintAddress = socketServer.recvfrom(2048)
+    print "received:", data, "from", clintAddress
+
+socketServer.close()
