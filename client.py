df7e7e9ca652d2432177c9f4b9f881b098b98ea1
zq9120 <zq9120@yeah.net>
2016-12-04 20:08:11 +0800

    update

--- client.py
+++ client.py
@@ -2,11 +2,11 @@ from scapy.all import *
 import time
 import os
 
-RANDOM_IP_POOL=['0.0.0.0/0']
+RANDOM_IP_POOL = ['0.0.0.0/0']
 
 
-def get_data():
-    data_prefix = "SDN_SPY_"
+def get_data(id):
+    data_prefix = "SDN_SPY_" + id + "_"
     return data_prefix + str(int(random.uniform(100000, 999999)))
 
 
@@ -19,10 +19,10 @@ def get_random_tos():
 
 
 def get_random_ip():
-    str_ip = RANDOM_IP_POOL[random.randint(0,len(RANDOM_IP_POOL) - 1)]
+    str_ip = RANDOM_IP_POOL[random.randint(0, len(RANDOM_IP_POOL) - 1)]
     str_ip_addr = str_ip.split('/')[0]
     str_ip_mask = str_ip.split('/')[1]
-    ip_addr = struct.unpack('>I',socket.inet_aton(str_ip_addr))[0]
+    ip_addr = struct.unpack('>I', socket.inet_aton(str_ip_addr))[0]
     mask = 0x0
     for i in range(31, 31 - int(str_ip_mask), -1):
         mask |= 1 << i
@@ -45,36 +45,37 @@ def send_udp(packet):
     sendp(packet)
     time.sleep(1)
 
+
 ip_dst = "10.0.0.1"
 mac_dst = "00:00:00:00:00:01"
 
 os.system("tcpdump -t udp -w ./client.pcap&>/dev/null")
 
 print "change dst port"
-pkt = Ether(dst=mac_dst)/IP(dst=ip_dst)/UDP(sport=9250, dport=get_random_port())/get_data()
+pkt = Ether(dst=mac_dst) / IP(dst=ip_dst) / UDP(sport=9250, dport=get_random_port()) / get_data("P1")
 send_udp(pkt)
 
 print "change src port"
-pkt = Ether(dst=mac_dst)/IP(dst=ip_dst)/UDP(sport=get_random_port(), dport=9250)/get_data()
+pkt = Ether(dst=mac_dst) / IP(dst=ip_dst) / UDP(sport=get_random_port(), dport=9250) / get_data("P2")
 send_udp(pkt)
 
 print "change ToS bits"
-pkt = Ether(dst=mac_dst)/IP(dst=ip_dst, tos=get_random_tos())/UDP(sport=9250, dport=9250)/get_data()
+pkt = Ether(dst=mac_dst) / IP(dst=ip_dst, tos=get_random_tos()) / UDP(sport=9250, dport=9250) / get_data("P3")
 send_udp(pkt)
 
 print "change src IP"
-pkt = Ether(dst=mac_dst)/IP(src=get_random_ip(), dst=ip_dst)/UDP(sport=9250, dport=9250)/get_data()
+pkt = Ether(dst=mac_dst) / IP(src=get_random_ip(), dst=ip_dst) / UDP(sport=9250, dport=9250) / get_data("P4")
 send_udp(pkt)
 
 print "change dst IP"
-pkt = Ether(dst=mac_dst)/IP(dst=get_random_ip())/UDP(sport=9250, dport=9250)/get_data()
+pkt = Ether(dst=mac_dst) / IP(dst=get_random_ip()) / UDP(sport=9250, dport=9250) / get_data("P5")
 send_udp(pkt)
 
 print "change src MAC"
-pkt = Ether(src=get_random_mac(), dst=mac_dst)/IP(dst=ip_dst)/UDP(sport=9250, dport=9250)/get_data()
+pkt = Ether(src=get_random_mac(), dst=mac_dst) / IP(dst=ip_dst) / UDP(sport=9250, dport=9250) / get_data("P6")
 send_udp(pkt)
 
-pkt = Ether(dst=mac_dst)/IP(dst=ip_dst)/UDP(sport=9250, dport=9250)/"SDN_SPY_exit"
+pkt = Ether(dst=mac_dst) / IP(dst=ip_dst) / UDP(sport=9250, dport=9250) / "SDN_SPY_exit"
 sendp(pkt)
 
 os.system("killall tcpdump")
