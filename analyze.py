76579e4c0ee621885ef017710b5df04c4e8b4043
zq9120 <zq9120@yeah.net>
2016-12-04 21:45:32 +0800

    update

--- analyze.py
+++ analyze.py
@@ -3,5 +3,21 @@ from scapy.all import *
 client_pacp = rdpcap("client.pcap")
 server_pacp = rdpcap("server.pcap")
 
-print len(client_pacp)
-print len(server_pacp)
+client_time = []
+server_time = []
+
+
+for i in range(0, len(client_pacp)):
+    packet = client_pacp[i]
+    if str(packet.load).startswith("SDN_SPY"):
+        print packet.time
+        client_time = packet.time
+
+for i in range(0, len(server_pacp)):
+    packet = client_pacp[i]
+    if str(packet.load).startswith("SDN_SPY"):
+        print packet.time
+        server_time = packet.time
+
+print "client_time_count: ", len(client_time)
+print "server_time_count: ", len(server_time)
