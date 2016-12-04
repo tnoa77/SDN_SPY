35a4fb3018d2b0f8ac96a5e9303a7e8b929f458d
zq9120 <zq9120@yeah.net>
2016-12-04 22:29:09 +0800

    update

--- analyze.py
+++ analyze.py
@@ -23,7 +23,7 @@ for i in range(0, len(server_pacp)):
 print "NO.\tP1_SEND\tP1_RECV\tP2_SEND\tP2_RECV"
 if len(client_time) == len(server_time):
     for i in range(0, len(client_time), 4):
-        print "D%d\t%2f\t%2f\t%2f\t%2f" % (
+        print "D%d\t%.2f\t%.2f\t%.2f\t%.2f" % (
             (i / 4 + 1), (server_time[i] - client_time[i]) * 100000000,
             (client_time[i + 1] - server_time[i + 1]) * 100000000,
             (server_time[i + 2] - client_time[i + 2]) * 100000000,
