2caf6e1e25783713601e21d47bc4dc05c0deaf23
zq9120 <zq9120@yeah.net>
2016-12-04 18:52:38 +0800

    update

--- server.py
+++ server.py
@@ -5,4 +5,4 @@ mac_dst = "00:00:00:00:00:02"
 while True:
     receive = sniff(filter="udp", count=1)
     if hasattr(receive[0], 'load') and str(receive[0].load).startswith("SDN_SPY_"):
-        receive[0].load
+        print receive[0].load
