4ca6d09346996eb2cc92699f1915f42afb1247b7
zq9120 <zq9120@yeah.net>
2016-12-04 22:34:55 +0800

    update

--- analyze.py
+++ analyze.py
@@ -20,11 +20,11 @@ for i in range(0, len(server_pacp)):
         print packet.time
         server_time.append(packet.time)
 
-print "%10s%13s%13s%13s%13s" % ("NO.", "P1_SEND,", "P1_RECV", "P2_SEND", "P2_RECV")
+print "%10s%12s%12s%12s%12s" % ("NO.", "P1_SEND", "P1_RECV", "P2_SEND", "P2_RECV")
 if len(client_time) == len(server_time):
     for i in range(0, len(client_time), 4):
-        print "D%9d%10.2f%10.2f%10.2f%10.2f" % (
-            (i / 4 + 1), (server_time[i] - client_time[i]) * 100000000,
+        print "%10s%12.2f%12.2f%12.2f%12.2f" % (
+            ("D" + str(i / 4 + 1)), (server_time[i] - client_time[i]) * 100000000,
             (client_time[i + 1] - server_time[i + 1]) * 100000000,
             (server_time[i + 2] - client_time[i + 2]) * 100000000,
             (client_time[i + 3] - server_time[i + 3]) * 100000000)
